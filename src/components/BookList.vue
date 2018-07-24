41e59fe8aed90d03dee62302757caa2215aa056a
Dan Yates <daniel.yates@idealshoppingdirect.co.uk>
2018-07-24 16:19:20 +0100

    Fixed it

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,13 +1,13 @@
 <template>
-	<div>
-		<h1>{{ title }}</h1>
-		<ul>
-			<book-item v-for="book in books" :book="book">
-			</book-item>
-		</ul>
+<div>
+	<h1>{{ title }}</h1>
+	<ul>
+		<book-item v-for="book in books" :book='book'>
+		</book-item>
+	</ul>
 
-		<book-form @addBook="appendBook"></book-form>
-	</div>
+	<book-form @addBook='appendBook'></book-form>
+</div>
 </template>
 
 <script>
@@ -16,25 +16,25 @@ import BookForm from './BookForm';
 
 export default {
   name: 'BookList',
-  data() {  	
-  	
-    return {
-      title: 'All Books',
-      books: [
-      {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
-	  {title: 'American Gods', author: 'Neil Gaiman'},
-	  {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},],
-	  components: { 
-	  	BookItem,
-	  	BookForm,
-	  },
-	  methods: {
-	  	appendBook(bookTitle, bookAuthor) {
-	  		this.books.push({ title: bookTitle, author: bookAuthor })
-	  	}
-	  }
-    };
+  components: { 
+  	BookItem,
+  	BookForm,
   },
+  methods: {
+  	appendBook(bookTitle, bookAuthor) {
+  		this.books.push({ title: bookTitle, author: bookAuthor })
+  	}
+  },
+  data() {
+	  return {  
+		  title: 'All Books',  
+		  books: [
+		  {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+		  {title: 'American Gods', author: 'Neil Gaiman'},
+		  {title: 'Amusing Ourselves to Death', author: 'Neil Postman'}
+		  ],  
+	  }
+	}	
 };
 </script>
 
