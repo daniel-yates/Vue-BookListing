9451956c5eb9ec409df1c2700b15cac267214e12
Dan Yates <daniel.yates@idealshoppingdirect.co.uk>
2018-07-24 12:05:01 +0100

    Looping through a list of books

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,16 +1,24 @@
 <template>
 	<div>
 		<h1>{{ title }}</h1>
-		<h2></h2>
+		<ul>
+			<li v-for="book in books">{{ book.title }} : {{ book.author }}</li>
+		</ul>
 	</div>
 </template>
 
 <script>
 export default {
   name: 'BookList',
-  data() {
+  data() {  	
+  	
     return {
       title: 'All Books',
+      books: [
+	    {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+	    {title: 'American Gods', author: 'Neil Gaiman'},
+	    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+		]
     };
   },
 };
@@ -19,5 +27,13 @@ export default {
 <style>
 	h1, h2 {
 		font-weight: normal;
+	}
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+	ul li {
+		display: block;
+		margin: 0 10px;
 	}
 </style>
